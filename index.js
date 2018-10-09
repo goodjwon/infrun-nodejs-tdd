fe21d5b85e402fb58516bf1ab8e3ee8ea243b0fc
goodjwon <jwon76@naver.com>
2018-10-09 12:17:02 +0900

    use should, use supertest

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.listen(3000, function () {
     console.log('server is running');
 })
 
-
+module.exports = app;
--- util.spec.js
+++ util.spec.js
@@ -1,9 +1,10 @@
 const utils = require('./util');
-const assert = require('assert');
+const should = require('should');
+
 
 describe('util.js module 에 대문자 함수', () => {
     it('문자열을 첫 문자를 대문자로 변경한다.', () => {
         const result = utils.captalise('hello');
-        assert.equal(result, 'Hello');
+        result.should.be.equal('Hello');
     })
 })
